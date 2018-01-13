5d66fe93a902ae2c7086351ccb048fb5fbb1dbd4
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-01-13 22:34:13 +0200

    I now make all integers into 4-digit format for easier handling

--- askisi4.py
+++ askisi4.py
@@ -1,13 +1,23 @@
-
 while True:
+    user_input = input("Roman Number Converter (Type 'exit' to quit)\nType in an integer up to 3999:\n")
+    if user_input == "exit":
+        break
     try:
-        user_input = int(input("Roman Number Converter\nType in an integer up to 3999:\n"))
+        check = int(user_input)
     except ValueError:
-        print("\n\nPlease, input an integer up to 3999!")
+        print("\nPlease, input an integer up to 3999!")
         continue
     else:
         if int(user_input) > 3999:
-            print("\n\nPlease, input an integer up to 3999!")
+            print("\nPlease, input an integer up to 3999!")
             continue
         print("well, that's an integer up to 3999, nice")
+        digits = list(user_input)
+        for i in range (4):
+            digits[i] = int(digits[i])
+        if len(digits) != 4:
+            for i in range (4 - len(digits)):
+                digits.insert(0, 0)
+        thousands = digits[0] * "M"
+        print(0 * ":P")
         break
