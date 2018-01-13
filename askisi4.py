184ebba201afd8ed8bab41419f97e8331d491d7c
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-01-13 23:19:31 +0200

    This version works as expected :)

--- askisi4.py
+++ askisi4.py
@@ -1,3 +1,12 @@
+def digit2roman(cur, one, five, ten):
+    if cur <= 3:
+        return cur * one
+    elif cur == 4:
+        return one + five
+    elif cur >= 5 and cur <= 8:
+        return five + (cur - 5) * one
+    else:
+        return one + ten
 while True:
     user_input = input("Roman Number Converter (Type 'exit' to quit)\nType in an integer up to 3999:\n")
     if user_input == "exit":
@@ -11,13 +20,13 @@ while True:
         if int(user_input) > 3999:
             print("\nPlease, input an integer up to 3999!")
             continue
-        print("well, that's an integer up to 3999, nice")
         digits = list(user_input)
-        for i in range (4):
-            digits[i] = int(digits[i])
         if len(digits) != 4:
             for i in range (4 - len(digits)):
                 digits.insert(0, 0)
-        thousands = digits[0] * "M"
-        print(0 * ":P")
-        break
+        thousands = int(digits[0]) * "M"
+        hundreds = digit2roman(int(digits[1]), "C", "D", "M")
+        tens = digit2roman(int(digits[2]), "X", "L", "C")
+        ones = digit2roman(int(digits[3]), "I", "V", "X")
+        print (thousands + hundreds + tens + ones + "\n")
+        continue
