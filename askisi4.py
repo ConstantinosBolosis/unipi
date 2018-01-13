d34aa009483f0f121d0f122bad124875965f57ac
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-01-13 21:59:03 +0200

    I now check user input, until I accept a valid integer

--- askisi4.py
+++ askisi4.py
@@ -1,1 +1,13 @@
 
+while True:
+    try:
+        user_input = int(input("Roman Number Converter\nType in an integer up to 3999:\n"))
+    except ValueError:
+        print("\n\nPlease, input an integer up to 3999!")
+        continue
+    else:
+        if int(user_input) > 3999:
+            print("\n\nPlease, input an integer up to 3999!")
+            continue
+        print("well, that's an integer up to 3999, nice")
+        break
