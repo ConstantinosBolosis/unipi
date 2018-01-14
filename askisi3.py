ceca1b60860d14a1e178c025352c0917df138142
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-01-14 17:28:44 +0200

    I know successfully add 13 to characters only. Not quite ROT13 yet

--- askisi3.py
+++ askisi3.py
@@ -1,8 +1,13 @@
 while True:
-    user_input = input("Feel free to write something: ")
-    for i in range (len(list(user_input))):
-        check = ord(list(user_input)[i])
-        if check > 126 or check < 32 :                    
+    user_input = list(input("Feel free to write something: "))
+    for i in range (len(user_input)):
+        num = ord(user_input[i])
+        if num > 126 or num < 32 :                    
             print("Can't process what you typed")
             break
-        
+        else:
+            if (num >= 65 and num <= 90) or (num >= 97 and num <= 122):
+                num += 13
+                user_input[i] = chr(num)
+    print("".join(user_input))
+
