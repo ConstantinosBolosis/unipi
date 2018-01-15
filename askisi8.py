e1673c57e8c566ad4c9a9a48490866f849fddf09
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-01-15 18:04:30 +0200

    Final Version

--- askisi8.py
+++ askisi8.py
@@ -1,16 +1,22 @@
+#Python 3.6 -- Constantinos Bolosis -- P17085 -- Combinations :/
+
 import random
-numlist = []
-for i in range (30):
-    numlist.append(random.randrange(-30, 30))
-print(numlist)
-combo = [None] * 3
-for x in range (28):
-    combo[0] = numlist[x]
-    for y in range (28 - x):
-        combo[1] = numlist[1 + x + y]
-        for z in range (28 - x - y):
-            combo[2] = numlist[2 + x + y + z]
-            if (combo[0] + combo[1] + combo[2] == 0):
-                print(combo)
-print(i)
-            
+while True:
+    numlist = random.sample(range(-30, 30), 30)
+    print(numlist, "\n", sep="")
+    combo = [None] * 3
+    i = 0
+    for x in range (28):
+        combo[0] = numlist[x]
+        for y in range (28 - x):
+            combo[1] = numlist[1 + x + y]
+            for z in range (28 - x - y):
+                combo[2] = numlist[2 + x + y + z]
+                if (combo[0] + combo[1] + combo[2] == 0):
+                   i += 1
+                   print(combo)
+    if i == 0:
+        print("There are no combinations of 3 elements from the main list that add up to 0")
+    meh = input("\nRun again? (y/n): ")
+    if meh == "n" or meh == "N":
+        break
