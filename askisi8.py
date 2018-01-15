7079f36e265df0fa775cc648478efc282b095438
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-01-15 15:08:58 +0200

    I now calculate the number of possible combinations

--- askisi8.py
+++ askisi8.py
@@ -3,3 +3,11 @@ numlist = list()
 for i in range (30):
     numlist.append(random.randrange(-30, 30))
 print(numlist)
+#triad = list()
+i = 0
+for x in range (28):
+    for y in range (28 - x):
+        for z in range (28 - x - y):
+            i += 1
+print(i)
+            
