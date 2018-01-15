f196f3f0c606814e04aa3d653ec9227cc15b721e
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-01-15 16:40:45 +0200

    This is pretty much working

--- askisi8.py
+++ askisi8.py
@@ -1,13 +1,16 @@
 import random
-numlist = list()
+numlist = []
 for i in range (30):
     numlist.append(random.randrange(-30, 30))
 print(numlist)
-#triad = list()
-i = 0
+combo = [None] * 3
 for x in range (28):
+    combo[0] = numlist[x]
     for y in range (28 - x):
+        combo[1] = numlist[1 + x + y]
         for z in range (28 - x - y):
-            i += 1
+            combo[2] = numlist[2 + x + y + z]
+            if (combo[0] + combo[1] + combo[2] == 0):
+                print(combo)
 print(i)
             
