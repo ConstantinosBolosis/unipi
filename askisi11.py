dfc9e16a08c9c315b8f2c57e41de9d0109be355c
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-02-09 18:39:11 +0200

    I know store the words from the input file and the generated square in lists for easier handling

--- askisi11.py
+++ askisi11.py
@@ -1,17 +1,26 @@
 import random
 import sys
 if len(sys.argv) != 2:
-    print("Not enough arguments given")
+    print("This script requires only one given argument")
 else:
     try:
         f = open(sys.argv[1], "r")
     except IOError:
         print("Can't open file", sys.argv[1])
     else:
+        words = [x.strip() for x in f.readlines()]
+        print(words)
         square = [[None] * 100] * 100
+        strings = [""] * 200
         for i in range (100):
             for j in range (100):
                 square[i][j] = chr(random.randrange(65,90))
+                strings[i] += square[i][j]
+                strings[100 + j] += square[i][j]
                 print(square[i][j], end="")
             print("\n", end="")
+        print(strings)
+
+
+        
         f.close()
