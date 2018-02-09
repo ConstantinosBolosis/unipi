aff081c2320c2935d98f6af6e7e0b810b1c807ca
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-02-09 20:39:04 +0200

    Final Version (optimised sqare generation, checks the dictionary)

--- askisi11.py
+++ askisi11.py
@@ -1,3 +1,5 @@
+#Python 3.6 -- Constantinos Bolosis -- P17085 -- 100x100 Square :D
+
 import random
 import sys
 if len(sys.argv) != 2:
@@ -6,21 +8,23 @@ else:
     try:
         f = open(sys.argv[1], "r")
     except IOError:
-        print("Can't open file", sys.argv[1])
+        print("Can't open", sys.argv[1], "or no such file exists")
     else:
         words = [x.strip() for x in f.readlines()]
-        print(words)
-        square = [[None] * 100] * 100
         strings = [""] * 200
         for i in range (100):
             for j in range (100):
-                square[i][j] = chr(random.randrange(65,90))
-                strings[i] += square[i][j]
-                strings[100 + j] += square[i][j]
-                print(square[i][j], end="")
-            print("\n", end="")
-        print(strings)
-
-
-        
+                char = chr(random.randrange(65,90))
+                strings[i] += char
+                strings[100 + j] += char
+            print(strings[i])
+        counter = 0
+        for i in range (len(words)):
+            if words[i] != "" and any(str.upper(words[i]) in square for square in strings):
+                counter += 1
+                if counter == 1:
+                    print("\nWords found:")
+                print(words[i])
+        if counter == 0:
+            print("\nNo words found")
         f.close()
