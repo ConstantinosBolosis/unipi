c57b87542c8085b0f885d6b4c476a58894a355b6
Constantinos Bolosis <p17085@students.cs.unipi.gr>
2018-02-09 16:36:38 +0200

    I now open dictionary file as argument (and check the integrity of the arguments given)

--- askisi11.py
+++ askisi11.py
@@ -1,7 +1,17 @@
 import random
-square = [[None] * 100] * 100
-for i in range (100):
-    for j in range (100):
-        square[i][j] = chr(random.randrange(65,90))
-        print(square[i][j], end="")
-    print("\n", end="")
+import sys
+if len(sys.argv) != 2:
+    print("Not enough arguments given")
+else:
+    try:
+        f = open(sys.argv[1], "r")
+    except IOError:
+        print("Can't open file", sys.argv[1])
+    else:
+        square = [[None] * 100] * 100
+        for i in range (100):
+            for j in range (100):
+                square[i][j] = chr(random.randrange(65,90))
+                print(square[i][j], end="")
+            print("\n", end="")
+        f.close()
